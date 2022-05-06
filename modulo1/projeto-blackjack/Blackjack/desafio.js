2a041278d3245dcbf981c58994400068acd3420b
avilacarol-bit <carolavelab@gmail.com>
2022-05-06 10:03:57 -0300

    primeira alteração do projeto blackjack

--- modulo1/projeto-blackjack/Blackjack/desafio.js
+++ modulo1/projeto-blackjack/Blackjack/desafio.js
@@ -29,26 +29,31 @@ if (confirm("Bem vinde ao jogo de Blackjack! \nQuer iniciar uma nova rodada?")){
          cartaUserUm = comprarCarta()
          cartaUserDois = comprarCarta()
          cartaUserTres = comprarCarta()
+         break
    } switch (cartasPc[0,1]){
       case "A":
          cartaPcUm = comprarCarta()
          cartaPcDois = comprarCarta()
          cartaPcTres = comprarCarta()
+         break
 
+   
    }if (confirm(`Suas cartas são ${cartaUserUm.texto} e ${cartaUserDois.texto}. A carta revelada do computador é ${cartaPcUm.texto}.\nDeseja comprar mais uma carta?`)){
-
+         let somarUser = 0
+         for(let i = 0; i < cartasUser.length; i++) {
+            somarUser += cartasUser[i].valor
+         }
+         let somarPc = 0
+         for(let i = 0; i < cartasPc.length; i++) {
+            somarPc += cartasPc[i].valor
+         }
+         alert(`Usuário - Cartas: ${cartaUserUm.texto}, ${cartaUserDois.texto} e ${cartaUserTres.texto}  - Pontuação: ${somarUser}.\nComputador - Cartas: ${cartaPcUm.texto}, ${cartaPcDois.texto} e ${cartaPcTres.texto} - Pontuação: ${somarPc}.`)
    }else{
       let somaUser = cartaUserUm.valor + cartaUserDois.valor
       let somaPc = cartaPcUm.valor + cartaPcDois.valor + cartaPcTres.valor
-      if (somaUser === somaPc){
-         let empate = ("Empate!")
-      }else if (somaUser > somaPc){
-         let usuario = ("O usuário ganhou!")
-      }else if (somaUser < somaPc){
-         let pc = ("O computador ganhou!")
-      }
-      alert(`Suas cartas são ${cartaUserUm.texto} e ${cartaUserDois.texto}. Sua pontuação é ${somaUser}.\nAs cartas do computador 
-      são ${cartaPcUm.texto}, ${cartaPcDois.texto} e ${cartaUserTres.texto}. A pontuação do computador é ${somaPc}.`)
+     
+
+      alert(`Suas cartas são ${cartaUserUm.texto} e ${cartaUserDois.texto}. Sua pontuação é ${somaUser}.\nAs cartas do computador são ${cartaPcUm.texto}, ${cartaPcDois.texto} e ${cartaUserTres.texto}. A pontuação do computador é ${somaPc}.`)
    }
       
 
