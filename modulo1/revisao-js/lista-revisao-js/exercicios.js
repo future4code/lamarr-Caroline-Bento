a2dbc104f65626aac1e0c848bd55d555517a0f4a
avilacarol-bit <carolavelab@gmail.com>
2022-05-11 21:21:25 -0300

    fazendo os exercicios

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -91,24 +91,30 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-    let numeros = array
-    let novosNumeros = []
     
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
    
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+   const pessoaAnonima = {
+       nome: "ANÔNIMO",
+   }
+   const novoObjetoAnonima = {
+       ...pessoa,
+       ...pessoaAnonima
+   }
+   return novoObjetoAnonima
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   ;
 }
 
 // EXERCÍCIO 13B
