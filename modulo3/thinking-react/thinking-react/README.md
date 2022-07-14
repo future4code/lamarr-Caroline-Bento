023e521ac57dd8692febda1780f53df3603bbf68
Caroline Ávila <102264452+avilacarol-bit@users.noreply.github.com>
2022-07-14 20:35:08 -0300

    thinking react

---
+++ modulo3/thinking-react/thinking-react/README.md
@@ -0,0 +1,2 @@
+# Árvore para construção do e-commerce
+<img src = "images/tree-components"/>
---
+++ modulo3/thinking-react/thinking-react/mockDeDados.js
@@ -0,0 +1,40 @@
+
+
+const mockData = [
+    {
+        id: 1,
+        product: 'Jogo PS4 - Spiderman: Miles Morales (Sony)',
+        cost: 'R$249,90',
+        photo: './images/spiderman.jpg',
+    },
+    {
+        id:2 ,
+        product: 'Jogo PS4 - Gran Turismo Sport (Sony)',
+        cost: 'R$99,90',
+        photo: './images/granturismo.jpg',
+    },
+    {
+        id:3,
+        product: 'Jogo PS4 - Friday the 13th: The Game',
+        cost: 'R$201,99',
+        photo: './images/friday13.jpg',
+    },
+    {
+        id: 4,
+        product: 'Jogo PS4 - Call of Duty: Infinite Warfare',
+        cost: 'R$150,00',
+        photo: './images/call-of-duty.jpg',
+    },
+    {
+        id: 5,
+        product: 'Jogo PS4 - Fifa 22',
+        cost: 'R$164,99',
+        photo: './images/fifa22.jpg',
+    },
+    {
+        id: 6,
+        product: 'Jogo PS4 - Mortal Kombat 11',
+        cost: 'R$99,90',
+        photo: './images/mortal-kombat.jpg',
+    }
+]
